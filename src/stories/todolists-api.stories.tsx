6586ceac7a43c189cf791f316cbc09a21d7031b0
annavilnid <anna.vilnid@gmail.com>
2023-10-21 21:01:58 +0200

    post todolist

--- src/stories/todolists-api.stories.tsx
+++ src/stories/todolists-api.stories.tsx
@@ -10,17 +10,19 @@ export const GetTodolists = () => {
     useEffect(() => {
         // здесь мы будем делать запрос и ответ закидывать в стейт.
         // который в виде строки будем отображать в div-ке
-    const promise = axios.get("https://social-network.samuraijs.com/api/1.1");
+    const promise = axios.get("https://social-network.samuraijs.com/api/1.1/todo-lists", {withCredentials: true});
     promise.then((resData) =>
-        setState(resData)
+        setState(resData.data)
     )}, [])
     return <div>{JSON.stringify(state)}</div>
 }
 export const CreateTodolist = () => {
     const [state, setState] = useState<any>(null)
     useEffect(() => {
-    }, [])
-
+        const promise = axios.post("https://social-network.samuraijs.com/api/1.1/todo-lists", {title: "newTodo"} , {withCredentials: true});
+        promise.then((resData) =>
+            setState(resData.data.data.item)
+        )}, [])
     return <div>{JSON.stringify(state)}</div>
 }
 export const DeleteTodolist = () => {
